8182e01a48dcff1b6a2b878dcc3163bedab0ff8f
cuterwrite <52732574+PKUcoldkeyboard@users.noreply.github.com>
2023-11-03 17:40:36 +0800

    Update func.py

--- func.py
+++ func.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
+import traceback
 import warnings
 warnings.filterwarnings('ignore')
 
