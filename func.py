c72d313538a0055af99e6f267512190b04704d21
jfplyl999 <50453655+jfplyl999@users.noreply.github.com>
2023-12-20 12:43:38 +0800

    Update func.py

--- func.py
+++ func.py
@@ -1,57 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import time
-import traceback
-import warnings
-warnings.filterwarnings('ignore')
 
-def login(driver, username, password, retry=0):
-    if retry == 3:
-        raise ValueError('login failed.')
-    
-    print('login...')
-    url = 'https://988660.cc/h5/index/login'
-    driver.get(url)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[4]/button[1]')))
-    driver.find_element_by_id('unm').send_keys(username)
-    time.sleep(0.1)
-    driver.find_element_by_id('pwd').send_keys(password)
-    time.sleep(0.1)
-    driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[3]/div[4]/button[1]').click()
-    try:
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[4]/button[1]')))
-        print('login succesfully.')
-    except:
-        print('Retrying...')
-        login(driver, username, password, retry+1)
-        
-    time.sleep(3)
-        
-        
-# 签到
-def checkin(driver):
-    try:
-        wait = WebDriverWait(driver, 10)
-        
-        # 使用WebDriverWait等待元素可见
-        button2 = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="v7"]/a/div[1]/img')))
-        
-        # 检查button是否有效，可点击
-        if button2.is_enabled():
-            button2.click()
-        else:
-            raise RuntimeError("Button2 is disabled or not clickable")
-        time.sleep(3)
-        print('签到成功！')
-    except Exception as e:
-        print('签到失败！')
-        traceback.print_exc()
-        
+# 创建一个Chrome浏览器实例
+driver = webdriver.Chrome()
 
-def run(driver, username, password):
-    login(driver, username, password)
-    checkin(driver)
-    
-if __name__ == '__main__':
+# 打开登录页面
+driver.get("https://8660ggg.cc/#/")
+
+# 第一步：检查是否有元素<uni-view data-v-d1f0eada="" class="task-close">
+try:
+    task_close = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "task-close"))
+    )
+    task_close.click()
+except:
     pass
+
+# 第二步：检查是否有元素<img src="./static/images/icon/login.png" draggable="false">
+try:
+    login_icon = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "img[src='./static/images/icon/login.png']"))
+    )
+    login_icon.click()
+except:
+    print("错误1")
+    driver.quit()
+    exit()
+
+# 第三步：检查是否有元素<input maxlength="140" step="" enterkeyhint="done" autocomplete="off" type="" class="uni-input-input">
+try:
+    username_input = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "uni-input-input"))
+    )
+    username_input.send_keys("username")
+except:
+    print("账号错误")
+    driver.quit()
+    exit()
+
+# 第四步：检查是否有元素<input maxlength="140" step="" enterkeyhint="done" autocomplete="off" type="password" class="uni-input-input">
+try:
+    password_input = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='password'][class='uni-input-input']"))
+    )
+    password_input.send_keys("password")
+except:
+    print("账号错误")
+    driver.quit()
+    exit()
+
+# 第五步：检查是否有元素<uni-view data-v-0d8a3f1a="" class="tologin">
+try:
+    tologin = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "tologin"))
+    )
+    tologin.click()
+except:
+    print("登录错误")
+    driver.quit()
+    exit()
+
+# 第六步：检查是否有元素<uni-view data-v-4d574c0c="" class="sign-close">
+try:
+    sign_close = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "sign-close"))
+    )
+    print("登录成功")
+except:
+    pass
+
+# 关闭浏览器
+driver.quit()
